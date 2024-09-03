013daec0b1ebbb5c32cac1013f2034383d672a1e
Aranda <aaranda21@cps.k12.il.us>
2024-09-03 12:28:46 -0500

    first commit

---
+++ lesson1.py
@@ -0,0 +1,23 @@
+#hey;)
+# print("hello my name is andy")
+# print("i go to hancock")
+# print("im learning python")
+name = input("what is your name? ")
+print("hello", name)
+# input allows us to take input from the user 
+age = input("how old are you?")
+print("you are"+ age+ "years old.")
+#the plus is a concatenation
+#ask the user for their age
+# print put the age
+color = input("what is ur fav color?")
+print("your fav color is"+ color".")
+# ask the user for their favorite color 
+# print out the fav color
+food = input("what is your fav food?")
+print("your fav food is"+ food+ ".")
+# ask the user thier fav food
+# print the fav food
+# concatenation is the process of combining strings
+fav_animal = input("whats ur fav animal?")
+print("your fav animal is"+ fav_animal+ ".")
